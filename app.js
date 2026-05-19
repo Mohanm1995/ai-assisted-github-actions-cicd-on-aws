39e9b040b26a2559267abeaba1dda503434c9d11
root <root@ip-172-31-32-5.ap-south-1.compute.internal>
2026-05-19 05:00:23 +0000

    Final Commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline Success');
+  res.send('Node App Deployed Successfully using GitHub Actions...!');
 });
 
 app.listen(3000, () => {
